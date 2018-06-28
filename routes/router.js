a2fa266a5c462cfaf1d785be183db57e2ab11377
Pierce <spammermuch@gmail.com>
2018-06-28 12:28:44 -0400

    Adding the request dep and removing body

--- routes/router.js
+++ routes/router.js
@@ -10,7 +10,7 @@ module.exports = async (app, db) => {
     });
 
     app.post('/register', (request, response) => {
-        var builder = creator(request.body);
+        var builder = creator(request.query);
 
         db.registered.find({"person.name": builder.person.name}, (err, data) => {
             if(err) {
